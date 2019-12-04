267152200b3f0db632b24011d8ac2c295909467e
am0n666 <bethor666@gmail.com>
2019-12-04 20:55:18 +0100

    xxx

--- install.sh
+++ install.sh
@@ -6,19 +6,15 @@
 
 dest_dir=/data/arm
 
-if [ ! -d $dest_dir ]
+if [ ! -d /data/arm ]
 then
-	mkdir -p $dest_dir
-	mkdir -p $dest_dir/sfs
-	mkdir -p $dest_dir/lib
-	mkdir -p $dest_dir/lib64
+	mkdir /data/arm
 fi
 
-cd $dest_dir/sfs
-
 # Houdini 7 - ARMv8 on x86_64
-wget -O houdini-7_z.sfs http://dl.android-x86.org/houdini/7_z/houdini.sfs 
+wget -O houdini64.sfs http://dl.android-x86.org/houdini/7_z/houdini.sfs
+mv houdini64.sfs /system/etc
 
 # Houdini 7 - ARMv7 on x86_64
-wget -O houdini-7_y.sfs http://dl.android-x86.org/houdini/7_y/houdini.sfs
-
+wget -O houdini.sfs http://dl.android-x86.org/houdini/7_y/houdini.sfs
+mv houdini.sfs /system/etc
